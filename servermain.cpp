e6300b656df4d2fdb4e4050204ce018c1b3d7a35
Sergei40 <seregagerasimov14@gmail.com>
2021-02-05 02:46:54 +0300

    read file 1

--- servermain.cpp
+++ servermain.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 int main (int argc, char* argv[]) {
     if (argc < 3) {
@@ -7,11 +8,21 @@ int main (int argc, char* argv[]) {
         return -1;
     }
     else {
-        fstream studentFile1 (argv[2]);
-        fstream studentFile2 (argv[3]);
+        std::ifstream studentFile1 (argv[2]);
+        std::ifstream studentFile2 (argv[3]);
 
         if (studentFile1.is_open() && studentFile2.is_open()) {
-             int a;
+            std::map <int, std::string> allStudentsMap;
+            std::string buffer;
+            while (!studentFile1.eof()) {
+                getline(studentFile1, buffer);
+                while (string[i] != ' ') {
+                    tmp 
+                } 
+                allStudentsMap[key] = value;
+                std::cout << allStudentsMap[key] << std::endl; 
+                break; 
+            }
         }
         else {
             std::cout << "files can't be opened\n";
